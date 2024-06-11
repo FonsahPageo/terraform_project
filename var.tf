d9b8967b3f69e30a7081d805d5cba0389f641283
FonsahPageo <ashprincepageo@gmail.com>
2024-06-11 13:03:57 +0100

    changed ingress protocol

--- var.tf
+++ var.tf
@@ -110,7 +110,7 @@ variable "sg_ingress_rules" {
     {
       from_port   = 22
       to_port     = 22
-      protocol    = "ssh"
+      protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     },
     {
